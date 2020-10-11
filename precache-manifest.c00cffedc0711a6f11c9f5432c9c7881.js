a4eb65fe95327784cde0cdd20c62fdebfbf36c7c
barz101 <barz100@gmail.com>
2020-10-11 22:01:31 +0300

    Updates

---
+++ precache-manifest.c00cffedc0711a6f11c9f5432c9c7881.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dd5038c95ce8211c1f9e98ef0bf24f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1892b561e6b39ca8448",
+    "url": "/static/css/main.48d72c20.chunk.css"
+  },
+  {
+    "revision": "ce98039fb44c4381d12a",
+    "url": "/static/js/2.2edecf94.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.2edecf94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1892b561e6b39ca8448",
+    "url": "/static/js/main.2a0c9dff.chunk.js"
+  },
+  {
+    "revision": "b11bca11519b71141567",
+    "url": "/static/js/runtime-main.2ae43783.js"
+  },
+  {
+    "revision": "d582391da9a68daf10a2ed2514c33826",
+    "url": "/static/media/Assistant-Bold.d582391d.ttf"
+  },
+  {
+    "revision": "f2bbc6bae2ee3ce641adc1bb1a655371",
+    "url": "/static/media/Assistant-ExtraBold.f2bbc6ba.ttf"
+  },
+  {
+    "revision": "e2b46dd69f54e57767ceef1d5fc8e688",
+    "url": "/static/media/Assistant-Regular.e2b46dd6.ttf"
+  },
+  {
+    "revision": "5fc99d6a29734bd4197d73bdeede68ab",
+    "url": "/static/media/lightbulb.5fc99d6a.svg"
+  },
+  {
+    "revision": "7a2a195042733393a712fa58b545e69d",
+    "url": "/static/media/more.7a2a1950.svg"
+  }
+]);
